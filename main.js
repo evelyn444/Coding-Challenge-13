8b73f036213f578f801e4d56555c176374757e29
Evelyn_ Hernandez <eh22@usf.edu>
2024-11-03 21:26:00 -0500

    Fetch product data and display basic information.

---
+++ main.js
@@ -0,0 +1,18 @@
+// Task 2:  Fetch Products from the API Using Fetch and Promises
+document.addEventListener('DOMContentLoaded', ()=> {
+    const productContainer = document.getElementById('productContainer');
+    const errorMessage = document.getElementById('errorMessage');
+
+    fetch('https://www.course-api.com/javascript-store-products')
+    .then(response => {
+        if (!response.ok){
+            throw new Error('Network Response is not working');
+        }
+        return response.json();
+    })
+    .then(data => {
+        displayProducts(data);
+    }) .catch(error => {
+
+    })
+})
